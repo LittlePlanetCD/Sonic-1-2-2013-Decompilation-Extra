1814c2b08b7492ed4f63c53ffac88350ddca54f8
LittlePlanetCD <littleplanetcd1993@gmail.com>
2023-04-12 17:28:06 -0500

    extra changes along with base repo

--- RSDKv4/RetroEngine.cpp
+++ RSDKv4/RetroEngine.cpp
@@ -1165,6 +1165,9 @@ bool RetroEngine::LoadGameConfig(const char *filePath)
             SetGlobalVariableByName("options.devMenuFlag", true);
         SetGlobalVariableByName("engine.standalone", true);
 #endif
+#if !RETRO_USE_ORIGINAL_CODE
+		SetGlobalVariableByName("isRemovedAds", true); // we're disabling ads for setups with no scripts (bytecode) to make it more in-line with script-enabled setups.
+#endif
     }
 
     // These need to be set every time its reloaded
